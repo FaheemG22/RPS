1d1e55cc1e87c104e53ffb90db94d7f13ea80be3
FaheemG22 <125843@shipley.ac.uk>
2023-01-30 18:05:58 +0000

    yes

--- main.py
+++ main.py
@@ -1,22 +1,82 @@
 from tkinter import *
 import random
-from threading import Thread
+from PIL import Image, ImageTk
 
 
 class RPS:
     def __init__(self):
         self.lives = 3
         self.wins = 0
-        Thread(target=self.display).start()
-        Thread(target=self.player).start()
 
-    def display(self):
         self.win = Tk()
+        self.win.geometry('250x180')
+        size = 80, 80
+
+        rock_image = Image.open('rock.png')
+        rock_image.thumbnail(size)
+        rock_image = ImageTk.PhotoImage(rock_image)
+        label_rock = Label(self.win, image=rock_image)
+        label_rock.image = rock_image
+        label_rock.grid(column=0, row=0)
+
+        paper_image = Image.open('paper.png')
+        paper_image.thumbnail(size)
+        paper_image = ImageTk.PhotoImage(paper_image)
+        label_paper = Label(self.win, image=paper_image)
+        label_paper.image = paper_image
+        label_paper.grid(column=1, row=0)
+
+        scissors_image = Image.open('scissors.png')
+        scissors_image.thumbnail(size)
+        scissors_image = ImageTk.PhotoImage(scissors_image)
+        label_scissors = Label(self.win, image=scissors_image)
+        label_scissors.image = scissors_image
+        label_scissors.grid(column=2, row=0)
+
+        rock = Button(self.win, text="Rock", height=5, width=10, command=lambda: self.choice('rock'))
+        rock.grid(column=0, row=1)
+
+        paper = Button(self.win, text="Paper", height=5, width=10, command=lambda: self.choice('paper'))
+        paper.grid(column=1, row=1)
+
+        scissors = Button(self.win, text="Scissors", height=5, width=10, command=lambda: self.choice('scissors'))
+        scissors.grid(column=2, row=1)
+
         self.win.mainloop()
 
-    def player(self):
-        while True:
-            choices = ['rock', 'paper', 'scissors']
-            comp_choice = random.choice(choices)
-            print(comp_choice)
+    def choice(self, play_choice):
+        choices = ['rock', 'paper', 'scissors']
+        comp_choice = random.choice(choices)
+        print(f'{play_choice}')
+        print(f'{comp_choice}\n')
+        # print(comp_choice)
+        if comp_choice == play_choice:
+            print('Tie :^|')
+        # player winning
+        elif comp_choice == 'rock' and play_choice == 'paper':
+            self.wins += 1
+        elif comp_choice == 'paper' and play_choice == 'scissors':
+            self.wins += 1
+        elif comp_choice == 'scissors' and play_choice == 'rock':
+            self.wins += 1
+
+        # player lost
+        elif comp_choice == 'rock' and play_choice == 'scissors':
+            self.lives -= 1
+        elif comp_choice == 'paper' and play_choice == 'rock':
+            self.lives -= 1
+        elif comp_choice == 'scissors' and play_choice == 'paper':
+            self.lives -= 1
+
+        if self.wins == 3:
+            print('winner')
+            self.win.destroy()
+        if self.lives == 0:
+            print('loser')
+            self.win.destroy()
+
+        print(f'lives: {self.lives}')
+        print(f'wins: {self.wins}\n')
+
+
 RPS()
